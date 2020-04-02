6f910f944cea90b128c1aefacf580dcc43e0c980
alekoxo <aleko@incendiaryblue.com>
2020-04-02 13:22:19 -0700

    added axios plugin

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,15 @@ export default {
 </script>
 
 <style lang="scss">
+body {
+  margin: 0px;
+  padding: 0px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,24 +9,21 @@ export default {
   name: 'Home',
   props: {
     msg: String
+  },
+  data() {
+    return{
+        covidData: null,
+    }
+  },
+  mounted () {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response))
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
