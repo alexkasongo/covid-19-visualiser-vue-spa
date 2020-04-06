6ec9f6db5c68aa55c07cb50cd891a3fcc4e38638
alekoxo <aleko@incendiaryblue.com>
2020-04-06 14:43:53 -0700

    summary chart logging working

--- src/components/data/Summary.vue
+++ src/components/data/Summary.vue
@@ -43,69 +43,87 @@ export default {
       .get('https://api.covid19api.com/summary')
       .then(response => {
         let summaryData = [];
+        let summaryDataFeed = []
+        let coloR = [];
+
+        var dynamicColors = function() {
+            var r = Math.floor(Math.random() * 255);
+            var g = Math.floor(Math.random() * 255);
+            var b = Math.floor(Math.random() * 255);
+            return "rgb(" + r + "," + g + "," + b + ")";
+         };
 
         const countries = []
         const totalConfirmed = []
         const totalDeaths = []
 
+        // initial data loop
         for (let country of response.data.Countries) {
             summaryData.push(country);
         }
-            
+        
+        // second data loop, goes deeper and allows selecting of countries, total deaths etc
         for (var i = 0; i < summaryData.length; i++) {
-          // console.log(`Summary.vue - 68 - variable`, summaryData[i]);
           countries.push(summaryData[i].Country)
           totalConfirmed.push(summaryData[i].TotalConfirmed)
           totalDeaths.push(summaryData[i].TotalDeaths)
+          coloR.push(dynamicColors());
         }
 
-        console.log(`Summary.vue - 70 - variable`, countries);
+        // NOTE currently not being used but should be used if app grows
+        // dispatch state/data to store for state manangenet
+        this.$store.dispatch("updateSummary", summaryDataFeed);
+
+        var chart = this.$refs.chart;
+        var ctx = chart.getContext("2d");
+        // eslint-disable-next-line no-unused-vars
+        var myChart = new Chart(ctx, {
+            type: 'horizontalBar',
+            data: {
+                labels: countries,
+                datasets: [{
+                    label: 'Number of Confirmed Cases',
+                    data: totalConfirmed,
+                    // data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: coloR,
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                },
+                // another line graph
+                { 
+                  label: 'Number of Confirmed Deaths',
+                  data: totalDeaths,
+                  backgroundColor: '#222',
+                  borderColor: [
+                    '#000',
+                  ],
+                  borderWidth: 2
+                }
+                ]
+            },
+            options: {
+              maintainAspectRatio: false,
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            padding: 25,
+                            autoSkip: false,
+                        }
+                    }]
+                }
+            }
+        });
+
       }
     );
-
-    // console.log(`Summary.vue - 73 - variable`, countries);
-
-    var chart = this.$refs.chart;
-      var ctx = chart.getContext("2d");
-      // eslint-disable-next-line no-unused-vars
-      var myChart = new Chart(ctx, {
-          type: 'bar',
-          data: {
-              labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-              // labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-              datasets: [{
-                  label: '# of Votes',
-                  data: [12, 19, 3, 5, 2, 3],
-                  backgroundColor: [
-                      'rgba(255, 99, 132, 0.2)',
-                      'rgba(54, 162, 235, 0.2)',
-                      'rgba(255, 206, 86, 0.2)',
-                      'rgba(75, 192, 192, 0.2)',
-                      'rgba(153, 102, 255, 0.2)',
-                      'rgba(255, 159, 64, 0.2)'
-                  ],
-                  borderColor: [
-                      'rgba(255,99,132,1)',
-                      'rgba(54, 162, 235, 1)',
-                      'rgba(255, 206, 86, 1)',
-                      'rgba(75, 192, 192, 1)',
-                      'rgba(153, 102, 255, 1)',
-                      'rgba(255, 159, 64, 1)'
-                  ],
-                  borderWidth: 1
-              }]
-          },
-          options: {
-              scales: {
-                  yAxes: [{
-                      ticks: {
-                          beginAtZero: true
-                      }
-                  }]
-              }
-          }
-      });
-        
   },
   computed: {
     ...mapGetters({
