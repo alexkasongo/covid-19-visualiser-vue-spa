535256e648c5e8909d94df56c751fc0a6e0abf61
alekoxo <aleko@incendiaryblue.com>
2020-04-09 22:57:37 -0700

    successfully added leaflet GeoJSON map

--- src/components/data/Map.vue
+++ src/components/data/Map.vue
@@ -1,25 +1,121 @@
 <template>
-  <div class="map">
-      <h1>Map</h1>
+
+  <div>
+    <div>
+      <span v-if="loading">Loading...</span>
+      <label for="checkbox">GeoJSON Visibility</label>
+      <input
+        id="checkbox"
+        v-model="show"
+        type="checkbox"
+      >
+      <label for="checkboxTooltip">Enable tooltip</label>
+      <input
+        id="checkboxTooltip"
+        v-model="enableTooltip"
+        type="checkbox"
+      >
+      <input
+        v-model="fillColor"
+        type="color"
+      >
+      <br>
+    </div>
+    <l-map
+      :zoom="zoom"
+      :center="center"
+      style="height: 500px; width: 100%"
+    >
+      <l-tile-layer
+        :url="url"
+        :attribution="attribution"
+      />
+      <l-geo-json
+        v-if="show"
+        :geojson="geojson"
+        :options="options"
+        :options-style="styleFunction"
+      />
+      <l-marker :lat-lng="marker" />
+    </l-map>
   </div>
 </template>
 
 <script>
+import { latLng } from "leaflet";
+import { LMap, LTileLayer, LMarker, LGeoJson } from "vue2-leaflet";
+import { Icon } from 'leaflet';
+
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
 export default {
-  name: 'Map',
+  name: "Example",
+  components: {
+    LMap,
+    LTileLayer,
+    LGeoJson,
+    LMarker
+  },
   data() {
-    return{
+    return {
+      loading: false,
+      show: true,
+      enableTooltip: true,
+      zoom: 6,
+      center: [48, -1.219482],
+      geojson: null,
+      fillColor: "#A52A2A",
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      marker: latLng(47.41322, -1.219482)
+    };
+  },
+  computed: {
+    options() {
+      return {
+        onEachFeature: this.onEachFeatureFunction
+      };
+    },
+    styleFunction() {
+      const fillColor = this.fillColor; // important! need touch fillColor in computed for re-calculate when change fillColor
+      return () => {
+        return {
+          weight: 2,
+          color: "#ECEFF1",
+          opacity: 1,
+          fillColor: fillColor,
+          fillOpacity: 1
+        };
+      };
+    },
+    onEachFeatureFunction() {
+      if (!this.enableTooltip) {
+        return () => {};
+      }
+      return (feature, layer) => {
+        layer.bindTooltip(
+          "<div>code:" +
+            feature.properties.code +
+            "</div><div>nom: " +
+            feature.properties.nom +
+            "</div>",
+          { permanent: false, sticky: true }
+        );
+      };
     }
   },
-  methods: {},
-  mounted () {}
-}
+  async created() {
+    this.loading = true;
+    const response = await fetch("https://rawgit.com/gregoiredavid/france-geojson/master/regions/pays-de-la-loire/communes-pays-de-la-loire.geojson")
+    const data = await response.json();
+    this.geojson = data;
+    this.loading = false;
+  }
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-.map { 
-    height: 600px; 
-    background-color: #eee;
-}
-</style>
--- src/components/data/Washington.vue
+++ src/components/data/Washington.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="washington">
     <div class="washington__stats">
-      <div class="washington__confirmed" v-for="event in filteredSum.slice(0, 1)" :key="event.id">
+      <div class="washington__confirmed" v-for="event in filteredSum" :key="event.id">
       <!-- <div class="washington__confirmed" v-for="event in filteredSum.slice(0, 1)" :key="event.id"> -->
         <div class="washington__washington-total">
           <h2>Total Cases</h2>
