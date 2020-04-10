4e52091c72bbe7709e64c99af19fab888faa04e3
alekoxo <aleko@incendiaryblue.com>
2020-04-09 22:46:47 -0700

    made leaflet system wide

--- src/main.js
+++ src/main.js
@@ -2,12 +2,26 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 
+/**
+ * moment and axios
+ */
 import axios from 'axios'
 import moment from 'moment'
-
 Vue.prototype.moment = moment
 Vue.prototype.$http = axios
 
+/**
+ * Leaflet map
+ */
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
+Vue.component('l-map', LMap);
+Vue.component('l-tile-layer', LTileLayer);
+Vue.component('l-marker', LMarker);
+
+/**
+ * Default
+ */
 Vue.config.productionTip = false
 
 new Vue({
